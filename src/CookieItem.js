95ff4eff330024595728204c954b6bc544a18f2a
myungho <skqoaudgh2@hanmail.net>
2020-04-12 13:16:07 +0900

    브라우저의 크기에 따라 CSS가 다르게 적용되도록 수정

--- src/CookieItem.js
+++ src/CookieItem.js
@@ -1,24 +1,35 @@
 import React from "react";
-import './Item.css';
+import "./Item.css";
 
 const CookieItem = ({ cookie }) => {
   function handleImageLoaded(event) {
-    event.target.classList.add('loaded');
+    event.target.classList.add("loaded");
   }
 
   return (
     <li className="listItem">
       <section className="item">
-        <figure className="imageWrapper"><img src={require(`./api/${cookie.imageURL}`)} alt={cookie.name} onLoad={handleImageLoaded} /></figure>
+        <figure className="imageWrapper">
+          <img
+            src={require(`./api/${cookie.imageURL}`)}
+            alt={cookie.name}
+            onLoad={handleImageLoaded}
+          />
+        </figure>
         <div className="infoWrapper">
-          <div className="info-title">{cookie.name} <strong>{cookie.grade}</strong> <span>짝꿍펫</span></div>
-          <ul className="info-detail"> 
+          <div className="info-title">
+            <div className="info-first">
+              <span className="itemName">{cookie.name}</span>
+              <strong>{cookie.grade}</strong>
+            </div>
+            <span className="partner">짝꿍펫</span>
+          </div>
+          <ul className="info-detail">
             <li>{cookie.health}</li>
-            <li>{cookie.skill} {cookie.candySkill}
-            </li>
             <li>
-              {cookie.unlockCondition}
+              {cookie.skill} {cookie.candySkill}
             </li>
+            <li>{cookie.unlockCondition}</li>
           </ul>
         </div>
       </section>
--- src/PetItem.js
+++ src/PetItem.js
@@ -1,17 +1,29 @@
 import React from "react";
-import './Item.css';
+import "./Item.css";
 
 const PetItem = ({ pet }) => {
   function handleImageLoaded(event) {
-    event.target.classList.add('loaded');
+    event.target.classList.add("loaded");
   }
 
   return (
     <li className="listItem">
       <section className="item">
-        <figure className="imageWrapper"><img src={require(`./api/${pet.imageURL}`)} alt={pet.name} onLoad={handleImageLoaded} /></figure>
+        <figure className="imageWrapper">
+          <img
+            src={require(`./api/${pet.imageURL}`)}
+            alt={pet.name}
+            onLoad={handleImageLoaded}
+          />
+        </figure>
         <div className="infoWrapper">
-          <div className="info-title">{pet.name} <strong>{pet.grade}</strong> <span>짝꿍쿠키</span></div>
+          <div className="info-title">
+            <div className="info-first">
+              <span className="itemName">{pet.name}</span>
+              <strong>{pet.grade}</strong>
+            </div>
+            <span className="partner">짝꿍쿠키</span>
+          </div>
           <div className="info-detail">{pet.skill}</div>
         </div>
       </section>
