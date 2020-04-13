4663bd4ced57d7f654dc5175927bfe7ab28bb55d
myungho <skqoaudgh2@hanmail.net>
2020-04-13 18:49:48 +0900

    모달 컴포넌트 출력 로직 수정

--- src/ModalCookie.js
+++ src/ModalCookie.js
@@ -4,74 +4,86 @@ import "./Modal.css";
 import icon_skill from "./assets/icon/magic.svg";
 import icon_bonus from "./assets/icon/bonus.svg";
 
-const ModalCookie = ({ pet, bonus, left, top }) => {
+const ModalCookie = ({ isOpen, pet, bonus, left, top }) => {
   const container = useRef();
-  const [position, setPosition] = useState({left: 0, top: 0});
+  const [position, setPosition] = useState({ left: 0, top: 0 });
 
   function handleImageLoaded(event) {
     event.target.classList.add("loaded");
   }
 
   useEffect(() => {
-    setPosition({
-      left: left - container.current.offsetWidth + 20,
-      top: top,
-      visibility: "visible"
-    });
+    if(container.current) {
+      setPosition({
+        left: left - container.current.offsetWidth + 20,
+        top: top,
+        visibility: "visible",
+      });
+    } 
   }, [left, top]);
 
   return (
-    <div ref={container} className="modal" style={position}>
-      <section className="modalItem">
-        <figure className="imageWrapper">
-          <img
-            src={require(`./assets/pet/${pet.imageURL}`)}
-            alt={pet.name}
-            onLoad={handleImageLoaded}
-          />
-        </figure>
-        <div className="infoWrapper">
-          <div className="info-title">
-            <div className="info-first">
-              <span className="itemName">{pet.name}</span>
-              <strong>{pet.grade}</strong>
-            </div>
-          </div>
-          <div className="info-detail">
-            <ul className="detailList">
-              <li>
-                <ul className="petSkills">
-                  {pet.skills.map((skill, index) => (
-                    <li key={index} className="petSkill">
-                      {index === 0 ? (
-                        <img
-                          src={icon_skill}
-                          alt="펫능력"
-                          title="펫능력"
-                          onLoad={handleImageLoaded}
-                        />
-                      ) : (
-                        <div style={{width:14, height: 10, display: 'inline-block'}}></div>
-                      )}
-                      {skill}
-                    </li>
-                  ))}
+    <React.Fragment>
+      {isOpen ? (
+        <div ref={container} className="modal" style={position}>
+          <section className="modalItem">
+            <figure className="imageWrapper">
+              <img
+                src={require(`./assets/pet/${pet.imageURL}`)}
+                alt={pet.name}
+                onLoad={handleImageLoaded}
+              />
+            </figure>
+            <div className="infoWrapper">
+              <div className="info-title">
+                <div className="info-first">
+                  <span className="itemName">{pet.name}</span>
+                  <strong>{pet.grade}</strong>
+                </div>
+              </div>
+              <div className="info-detail">
+                <ul className="detailList">
+                  <li>
+                    <ul className="petSkills">
+                      {pet.skills.map((skill, index) => (
+                        <li key={index} className="petSkill">
+                          {index === 0 ? (
+                            <img
+                              src={icon_skill}
+                              alt="펫능력"
+                              title="펫능력"
+                              onLoad={handleImageLoaded}
+                            />
+                          ) : (
+                            <div
+                              style={{
+                                width: 14,
+                                height: 10,
+                                display: "inline-block",
+                              }}
+                            ></div>
+                          )}
+                          {skill}
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                  <li>
+                    <img
+                      src={icon_bonus}
+                      alt="조합보너스"
+                      title="조합보너스"
+                      onLoad={handleImageLoaded}
+                    />
+                    {bonus}
+                  </li>
                 </ul>
-              </li>
-              <li>
-                <img
-                  src={icon_bonus}
-                  alt="조합보너스"
-                  title="조합보너스"
-                  onLoad={handleImageLoaded}
-                />
-                {bonus}
-              </li>
-            </ul>
-          </div>
+              </div>
+            </div>
+          </section>
         </div>
-      </section>
-    </div>
+      ) : null}
+    </React.Fragment>
   );
 };
 
