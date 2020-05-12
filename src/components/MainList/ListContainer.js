8c6d4bdd7051f75d293c33aa433bf75e77254fbd
myungho <skqoaudgh2@hanmail.net>
2020-05-12 17:16:17 +0900

    필터링이 잘 동작하지 않던 문제 수정

--- src/components/MainList/ListContainer.js
+++ src/components/MainList/ListContainer.js
@@ -9,25 +9,40 @@ const ListContainer = () => {
     ListContext
   );
 
-  let filteredCookies = filtering(cookies, search, grade, order);
-  filteredCookies = filteredCookies.map((v) => {
-    return { ...v, pet: pets[v.partner] };
-  });
-  let filteredPets = filtering(
-    pets.map((v) => {
-      return {
-        ...v,
-        release: v.release ? v.release : cookies[v.partner].release,
-      };
-    }),
-    search,
-    grade,
-    order
-  );
-  filteredPets = filteredPets.map((v) => {
-    return { ...v, cookie: cookies[v.partner] };
-  });
-  let filteredTreasures = filtering(treasures, search, grade, order);
+  let filteredCookies =
+    type === 'allType' || type === 'cookie'
+      ? filtering(cookies, search, grade, order)
+      : cookies;
+  filteredCookies =
+    type === 'allType' || type === 'cookie'
+      ? filteredCookies.map((v) => {
+          return { ...v, pet: pets[v.partner] };
+        })
+      : filteredCookies;
+  let filteredPets =
+    type === 'allType' || type === 'pet'
+      ? filtering(
+          pets.map((v) => {
+            return {
+              ...v,
+              release: v.release ? v.release : cookies[v.partner].release,
+            };
+          }),
+          search,
+          grade,
+          order
+        )
+      : pets;
+  filteredPets =
+    type === 'allType' || type === 'pet'
+      ? filteredPets.map((v) => {
+          return { ...v, cookie: cookies[v.partner] };
+        })
+      : filteredPets;
+  let filteredTreasures =
+    type === 'allType' || type === 'treasure'
+      ? filtering(treasures, search, grade, order)
+      : treasures;
 
   return (
     <main>
